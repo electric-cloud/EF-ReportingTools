81bafe969b3bbc71fa77d30a3713a39e944658a5
Greg Maxey <gregorymaxey@yahoo.com>
2018-04-05 16:16:02 -0400

    Fix plugin view issu

--- ec_setup.pl
+++ ec_setup.pl
@@ -1,10 +1,11 @@
 use Cwd;
 use File::Spec;
 use POSIX;
-use Archive::Zip;
 use MIME::Base64;
-use Digest::MD5 qw(md5_hex);
 use File::Temp qw(tempfile tempdir);
+use Archive::Zip;
+use Digest::MD5 qw(md5_hex);
+
 my $dir = getcwd;
 my $logfile ="";
 my $pluginDir;
@@ -19,7 +20,7 @@ else {
     $pluginDir = File::Spec->catfile($commanderPluginDir, $pluginName);
 }
 
-$logfile .= "Plugin directory is $pluginDir\n";
+$logfile .= "Plugin directory is $pluginDir";
 
 $commander->setProperty("/plugins/$pluginName/project/pluginDir", {value=>$pluginDir});
 $logfile .= "Plugin Name: $pluginName\n";
@@ -27,6 +28,13 @@ $logfile .= "Current directory: $dir\n";
 
 # Evaluate promote.groovy or demote.groovy based on whether plugin is being promoted or demoted ($promoteAction)
 local $/ = undef;
+# If env variable QUERY_STRING exists:
+my $dslFilePath;
+if(defined $ENV{QUERY_STRING}) { # Promotion through UI
+    $dslFilePath = File::Spec->catfile($ENV{COMMANDER_PLUGINS}, $pluginName, "dsl", "$promoteAction.groovy");
+} else {  # Promotion from the command line
+    $dslFilePath = File::Spec->catfile($pluginDir, "dsl", "$promoteAction.groovy");
+}
 
 my $demoteDsl = q{
 # demote.groovy placeholder
@@ -45,7 +53,6 @@ else {
   $dsl = $demoteDsl;
 }
 
-
 my $dslReponse = $commander->evalDsl(
     $dsl, {
         parameters => qq(
@@ -62,12 +69,10 @@ my $dslReponse = $commander->evalDsl(
 
 
 $logfile .= $dslReponse->findnodes_as_string("/");
-
 my $errorMessage = $commander->getError();
+
 if ( !$errorMessage ) {
-
     # This is here because we cannot do publishArtifactVersion in dsl today
-
     # delete artifact if it exists first
 
     my $dependenciesProperty = '/projects/@PLUGIN_NAME@/ec_groovyDependencies';
@@ -104,42 +109,47 @@ if ( !$errorMessage ) {
       }
     }
 
-    my $binary = decode_base64($base64);
-    my ($tempFh, $tempFilename) = tempfile(CLEANUP => 1);
-    binmode($tempFh);
-    print $tempFh $binary;
-    close $tempFh;
+    if ($base64) {
+      my $grapesVersion = '1.0.0';
+      my $cleanup = 1;
+      my $groupId = 'com.electriccloud';
+      $commander->deleteArtifactVersion($groupId . ':@PLUGIN_KEY@-Grapes:' . $grapesVersion);
+      my $binary = decode_base64($base64);
+      my ($tempFh, $tempFilename) = tempfile(CLEANUP => $cleanup);
+      binmode($tempFh);
+      print $tempFh $binary;
+      close $tempFh;
 
-    my ($tempDir) = tempdir(CLEANUP => 1);
-    my $zip = Archive::Zip->new();
-    unless($zip->read($tempFilename) == Archive::Zip::AZ_OK()) {
-      die "Cannot read .zip dependencies: $!";
-    }
-    $zip->extractTree("", File::Spec->catfile($tempDir, ''));
+      my ($tempDir) = tempdir(CLEANUP => $cleanup);
+      my $zip = Archive::Zip->new();
+      unless($zip->read($tempFilename) == Archive::Zip::AZ_OK()) {
+        die "Cannot read .zip dependencies: $!";
+      }
+      $zip->extractTree("", File::Spec->catfile($tempDir, ''));
 
-
-    if ( $promoteAction eq "promote" ) {
-
-        #publish jars to the repo server if the plugin project was created successfully
-        my $am = new ElectricCommander::ArtifactManagement($commander);
-        my $artifactVersion = $am->publish(
+      if ( $promoteAction eq "promote" ) {
+          #publish jars to the repo server if the plugin project was created successfully
+          my $am = new ElectricCommander::ArtifactManagement($commander);
+          my $artifactVersion = $am->publish(
               {   groupId         => $groupId,
                   artifactKey     => '@PLUGIN_KEY@-Grapes',
                   version         => $grapesVersion,
-                includePatterns => "**",
-                fromDirectory   => "$tempDir/lib",
-                description => 'JARs that @PLUGIN_KEY@ plugin procedures depend on'
-            }
-        );
+                  includePatterns => "**",
+                  fromDirectory   => "$tempDir/lib/grapes",
+                  description => 'JARs that @PLUGIN_KEY@ plugin procedures depend on'
+              }
+          );
 
-        # Print out the xml of the published artifactVersion.
-        $logfile .= $artifactVersion->xml() . "\n";
-
-        if ( $artifactVersion->diagnostics() ) {
-            $logfile .= "\nDetails:\n" . $artifactVersion->diagnostics();
-        }
+          # Print out the xml of the published artifactVersion.
+          $logfile .= $artifactVersion->xml() . "\n";
+          if ( $artifactVersion->diagnostics() ) {
+              $logfile .= "\nDetails:\n" . $artifactVersion->diagnostics();
+          }
+      }
     }
 }
+
+
 # Create output property for plugin setup debug logs
 my $nowString = localtime;
 $commander->setProperty( "/plugins/$pluginName/project/logs/$nowString", { value => $logfile } );
